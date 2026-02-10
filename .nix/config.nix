7c5d6263accfff18c0c26db9da8ad0b75c415328
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-02-10 21:08:41 +0100

    Nix CI

---
+++ .nix/config.nix
@@ -0,0 +1,28 @@
+{
+  ## DO NOT CHANGE THIS
+  format = "1.0.0";
+  ## unless you made an automated or manual update
+  ## to another supported format.
+
+  attribute = "CakeMLExtraction";
+
+  no-rocq-yet = true;
+
+  default-bundle = "9.0";
+
+  bundles."9.0" = {
+    coqPackages.coq.override.version = "9.0";
+    rocqPacakges.rocq-core.override.version = "9.0";
+    coqPackages.metarocq.override.version = "1.4-9.0";
+    coqPackages.ceres.override.version = "dev";
+  };
+
+  bundles."9.0".push-branches = ["main"];
+
+  cachix.coq = {};
+  cachix.math-comp = {};
+  cachix.coq-community = {};
+  cachix.metarocq = {};
+
+  cachix.peregrine.authToken = "CACHIX_AUTH_TOKEN";
+}
