f4129d82edb1ddb1e318ba947ea8bd4b2c9e70c2
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-03-12 22:26:08 +0100

    Relax Rocq and MetaRocq version constraints

--- .nix/config.nix
+++ .nix/config.nix
@@ -8,8 +8,19 @@
 
   no-rocq-yet = true;
 
-  default-bundle = "9.1";
+  default-bundle = "9.0";
 
+  bundles."9.0" = {
+    coqPackages = {
+      coq.override.version = "9.0";
+      metarocq-utils.override.version = "1.4-9.0.1";
+      metarocq-erasure-plugin.override.version = "1.4-9.0.1";
+      ceres-bs.override.version = "1.0.0";
+    }; rocqPackages = {
+      rocq-core.override.version = "9.0";
+    };
+    push-branches = [ "main" "rocq-9.1" ];
+  };
   bundles."9.1" = {
     coqPackages = {
       coq.override.version = "9.1";
